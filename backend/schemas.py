ba45510ceb36044d99c9e2d73a064ca457529cc2
dilademirhan <diladmrhn@gmail.com>
2026-04-19 21:57:19 +0300

    feat: add pydantic schemas for registration and login validation #4

---
+++ backend/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
+
+class UserCreate(BaseModel):
+    full_name: str = Field(..., min_length=5, max_length=100)
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+
+    @field_validator('email')
+    @classmethod
+    def email_domain_check(cls, v: str) -> str:
+        if not v.endswith('@arel.edu.tr'):
+            raise ValueError('Only @arel.edu.tr emails are accepted.')
+        return v
+
+    @field_validator('password')
+    @classmethod
+    def password_complexity_check(cls, v: str) -> str:
+        if not any(char.isupper() for char in v):
+            raise ValueError('Must contain an uppercase letter.')
+        if not any(char.islower() for char in v):
+            raise ValueError('Must contain a lowercase letter.')
+        if not any(char.isdigit() for char in v):
+            raise ValueError('Must contain a digit.')
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", v):
+            raise ValueError('Must contain a special character.')
+        return v
+
+class UserResponse(BaseModel):
+    user_id: int
+    full_name: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
